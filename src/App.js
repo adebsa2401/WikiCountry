7b18fc5a973b1a0b2f9e40c81e5d9d40e30a9c00
Ben Sales <adebsa2401@gmail.com>
2023-01-21 05:48:49 +0100

    fix: setup eslintrc file

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import {Provider} from 'react-redux';
+import {
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+  Navigate,
+  createHashRouter,
+} from 'react-router-dom';
+import DetailsPage from './pages/DetailsPage';
+import HomePage from './pages/HomePage';
+import store from './redux/configureStore';
+import './styles/App.css';
+
+export default function App() {
+  const router = createHashRouter(
+    createRoutesFromElements(
+      <Route path="/">
+        <Route path="countries" element={<HomePage />} />
+        <Route path="countries/:iso2/cities" element={<DetailsPage />} />
+        <Route path="" element={<Navigate to="countries" replace />} />
+      </Route>,
+    ),
+  );
+
+  return (
+    <Provider store={store}>
+      <RouterProvider router={router} />
+    </Provider>
+  );
+}
---
+++ src/components/CityItem.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import {AiFillStar} from 'react-icons/ai';
+import '../styles/CityItem.css';
+import {Text, View} from 'react-native';
+
+export default function CityItem({name, statistic, isCapital, index}) {
+  return (
+    <View className={`city-item ${index % 2 === 0 ? 'city-item-even' : ''}`}>
+      <Text className="city-name">{name}</Text>
+      {isCapital && (
+        <AiFillStar style={{color: 'yellow', marginRight: 'auto'}} />
+      )}
+      <Text>{new Intl.NumberFormat().format(statistic)}</Text>
+    </View>
+  );
+}
+
+CityItem.propTypes = {
+  name: PropTypes.string.isRequired,
+  statistic: PropTypes.number.isRequired,
+  isCapital: PropTypes.bool,
+  index: PropTypes.number,
+};
+
+CityItem.defaultProps = {
+  isCapital: false,
+  index: 0,
+};
